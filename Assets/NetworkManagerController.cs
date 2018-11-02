d9529a4e9fc43b569de1f5592c5c2a84e39c78a5
Ajay Kotian <005411285@coyote.csusb.edu>
2018-11-02 11:53:51 -0700

    Added scripts to enable networking in Oculus Go

---
+++ Assets/NetworkManagerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class NetworkManagerController : NetworkBehaviour {
+
+    public NetworkManager NetworkManagerRef;
+
+    public bool IsServer = false;
+
+	// Use this for initialization
+	void Start () {
+        if (!IsServer)
+        {
+           this.NetworkManagerRef.StartClient();
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
